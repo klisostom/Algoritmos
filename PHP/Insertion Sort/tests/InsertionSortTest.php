5c4c7f65774d297056171fd03ea7005e1f4559c2
Klisostom <klis@pm.me>
2021-09-15 22:44:26 -0300

    test(php)
    Create InsertionSortTest

---
+++ PHP/Insertion Sort/tests/InsertionSortTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+
+require_once __ROOT__ . '../vendor/autoload.php';
+
+final class InsertionSortTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function insertion(): void
+    {
+        # Arrange
+        $insertion = new InsertionSort();
+        $array = [];
+
+        for ($i=0; $i < random_int(5, 15); $i++) {
+            $array[] = $i;
+        }
+
+        $obj = new RandomizeInPlace();
+        $permutation = $obj->permute($array);
+
+        # Act
+        $actual = $insertion->sort($permutation);
+
+        # Assert
+        $this->assertNotEquals($array, $permutation);
+
+        $this->assertEquals(
+            $array,
+            $actual,
+            'The arrays would must be equals.'
+        );
+    }
+
+    /** @test */
+    public function insertionDesc(): void
+    {
+        # Arrange
+        $insertion = new InsertionSort();
+        $array = [];
+
+        for ($i = random_int(5, 15); $i >= 0 ; $i--) {
+            $array[] = $i;
+        }
+
+        $obj = new RandomizeInPlace();
+        $permutation = $obj->permute($array);
+
+        # Act
+        $actual = $insertion->sortDesc($permutation);
+
+        # Assert
+        $this->assertEquals(
+            $array,
+            $actual,
+            'The arrays dont are equals in desc way.'
+        );
+    }
+}
