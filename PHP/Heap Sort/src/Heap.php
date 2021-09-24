e626588965b03741a7ffe86fcba9e3daff4a1155
Klisostom <klis@pm.me>
2021-09-23 21:08:06 -0300

    feat(php): Adicionando um ao valor do cálculo de left e right. Implementação do maxHeapify.

--- PHP/Heap Sort/src/Heap.php
+++ PHP/Heap Sort/src/Heap.php
@@ -13,11 +13,36 @@ class Heap
 
     public function left(int $i): int
     {
-        return $i << 1;
+        return ($i << 1) + 1;
     }
 
     public function right(int $i): int
     {
-        return ($i << 1) + 1;
+        return ($i << 1) + 2;
+    }
+
+    public function maxHeapify(array &$A, int &$index): void
+    {
+        $left = $this->left($index);
+        $right = $this->right($index);
+        $largest = null;
+
+        if ($left < count($A) && $A[$left] > $A[$index]) {
+            $largest = $left;
+        } else {
+            $largest = $index;
+        }
+
+        if ($right < count($A) && $A[$right] > $A[$largest]) {
+            $largest = $right;
+        }
+
+        if ($largest != $index) {
+            $temp = $A[$largest];
+            $A[$largest] = $A[$index];
+            $A[$index] = $temp;
+
+            $this->maxHeapify($A, $largest);
+        }
     }
 }
